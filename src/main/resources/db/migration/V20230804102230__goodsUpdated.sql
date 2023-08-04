8d030ae6dcb667d8d5a30ce20a6124d46e74fa01
klov14 <135830622+klov14@users.noreply.github.com>
2023-08-04 17:33:19 +0600

    ExtraInfoForGoods

---
+++ src/main/resources/db/migration/V20230804102230__goodsUpdated.sql
@@ -0,0 +1,8 @@
+Alter table goods1
+    ADD COLUMN created_user varchar;
+Alter table goods1
+    ADD COLUMN created_date timestamp;
+Alter table goods1
+    ADD COLUMN updated_user varchar;
+Alter table goods1
+    ADD COLUMN updated_date timestamp;
