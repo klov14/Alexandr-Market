1549842bfb633e87149058395ae5cccdec9215a6
klov14 <135830622+klov14@users.noreply.github.com>
2023-08-09 11:10:24 +0600

    Clearing

--- src/main/java/com/example/ProjectGoods/model/Category.java
+++ src/main/java/com/example/ProjectGoods/model/Category.java
@@ -1,13 +1,9 @@
 package com.example.ProjectGoods.model;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
-
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -46,5 +42,4 @@ public class Category {
     @OneToMany(mappedBy = "categoryMapping")
     @JsonIgnore
     private Set<Good> goods = new HashSet<>();
-
 }
